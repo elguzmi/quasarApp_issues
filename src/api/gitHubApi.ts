b31d8b16f369310b414fe4590c170f6abc03e1f5
Santiago Guzman <superteso567@hotmail.com>
2024-11-18 07:15:41 -0500

    adding: token

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const gitHubApi = axios.create({
   baseURL: `https://api.github.com/repos/${process.env.GITHUB_USER}/${process.env.GITHUB_REPO}`,
   headers: {
-    //Authorization: `token ${process.env.GITHUB_TOKEN}`,
+    Authorization: `token ${process.env.GITHUB_TOKEN}`, //todas las peticiones van a llevar ese token de acceso
   },
 });
